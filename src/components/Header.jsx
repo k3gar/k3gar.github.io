51e275a92244d4bd7a5737d5146bbf80aef4a177
Kevin Garcia <kevinede40@gmail.com>
2021-11-28 14:55:53 -0600

    Adding Function to Hamburger Menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {Component} from 'react';
 //import menuLogo from './assets/menu.png';
-import menuLogo from './img/menu_black_prueba.svg'
+//import menuLogo from './img/menu_black_prueba.svg'
 import avatar from './img/flork-white.svg'
 
 /* const navMenu = document.querySelector(".nav-menu");
@@ -12,28 +12,41 @@ hamburger.addEventListener("click", activarMenu);
 function activarMenu(){
 navMenu.classList.toggle("active");
 } */
-export const Header = () => {
-    
+
+function prueba(){
+    const navMenu = document.querySelector(".nav-menu");
+    const hamburger = document.querySelector(".hamburger");
+    const bar = document.querySelectorAll("span.bar");
+
+    hamburger.addEventListener("click", [navMenu.classList.toggle("active"), console.log('Click')]);
+
+    /* hamburger.addEventListener("click", activarMenu);
+    function activarMenu(){
+    navMenu.classList.toggle("active");
+    } */
+}
+
+export const Header = () => {   
     return (
         <header>
-            {/* <img src={menuLogo} alt="Prueba" className='menu-button' /> */}
-            <div class="hamburger">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
+            {/* <img src={menuLogo} alt="Prueba" className='hamburger' onClick={prueba} /> */}
+            <div className="hamburger" onClick={prueba}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
             </div>
             <ul className="nav-menu">
                 <li className="nav-item">
-                    <a href="#" className="nav-link">Home</a>
+                    <a href="https://www.facebook.com/" className="nav-link">Home</a>
                 </li>
                 <li className="nav-item">
-                    <a href="#" className="nav-link">About</a>
+                    <a href="https://www.facebook.com/" className="nav-link">About</a>
                 </li>
                 <li className="nav-item">
-                    <a href="#" className="nav-link">Blog</a>
+                    <a href="https://www.facebook.com/" className="nav-link">Blog</a>
                 </li>
                 <li className="nav-item">
-                    <a href="#" className="nav-link">Projects</a>
+                    <a href="https://www.facebook.com/" className="nav-link">Projects</a>
                 </li>
                 <img src={avatar} alt="Flork saying hi" className="avatar-menu"/>
             </ul>
